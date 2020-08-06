5a68484295536ec775a0a7e65d51a910f21ee289
Maurici Abad Gutierrez <hello@mauriciabad.com>
2020-08-06 18:04:21 +0200

    Fix warnings 2

--- src/views/GameView.jsx
+++ src/views/GameView.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import HeaderFooter from '../components/HeaderFooter';
 import useStyles from './GameViewStyles';
 import { GameContext } from '../contexts/GameContext';
