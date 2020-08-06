e03cf1b868679a4ca99b2bca21d22eff8e61e0ba
mauriciabad <hello@mauriciabad.com>
2020-08-06 16:52:21 +0200

    Create view

--- src/views/GameNewView.jsx
+++ src/views/GameNewView.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import HeaderFooter from '../components/HeaderFooter';
 import useStyles from './GameNewViewStyles';
+import { TextField, Paper, Button, Typography } from '@material-ui/core';
+import { GameDispatchContext } from '../contexts/GameContext';
+import { useHistory } from 'react-router-dom';
 
 export default function GameNewView() {
   const classes = useStyles();
+  let [cardsString, setCardsString] = useState('');
+  const dispatchGame = useContext(GameDispatchContext);
+  const history = useHistory();
+
+
+  function handleCardsStringChange(e) {
+    setCardsString(e.target.value);
+  }
+
+  function handleCreateClick() {
+    const orderedCards = cardsString
+      .split('\n')
+      .map((val) => val.trim())
+      .filter((val) => val !== '');
+
+      dispatchGame({type: 'beginGame', payload: { orderedCards }});
+      history.replace('/game');
+  }
 
   return (
     <HeaderFooter className={classes.root}>
-      <p>Game new view</p>
+      <Paper className={classes.paper}>
+        <Typography variant="h4">Emoji list</Typography>
+        <Typography variant="body1">Each line is a card</Typography>
+        <TextField
+          id="outlined-multiline-static"
+          multiline
+          rows={10}
+          defaultValue={cardsString}
+          variant="outlined"
+          className={classes.textArea}
+          value={cardsString} onChange={handleCardsStringChange}
+        />
+      </Paper>
+      <Button variant="contained" color="primary" onClick={handleCreateClick}>Create</Button>
     </HeaderFooter>
   );
 }
--- src/views/GameNewViewStyles.tsx
+++ src/views/GameNewViewStyles.tsx
@@ -3,4 +3,11 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core';
 export default makeStyles((theme: Theme) => createStyles({
   root: {
   },
+  paper: {
+    margin: `${theme.spacing(2)}px auto`,
+    padding: theme.spacing(2),
+  },
+  textArea: {
+    width: '100%',
+  },
 }));
